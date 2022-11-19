c4b2a0fd38ef0d73a77e7e9b0e6ab51165a86a99
beetrbgus <devbeetrb0214@gmail.com>
2022-11-20 01:46:15 +0900

    ➕ [ADD] : 규현 - 1주차 문제풀이
    ## <a href="https://www.acmicpc.net/problem/17413"> 단어 뒤집기2 </a>
    - 17413 번 단어 뒤집기 문제
    - 초기 답안과 약간의 refactoring

---
+++ 황규현/문제/1주차/B_17413_WordReverse2_2.java
@@ -0,0 +1,58 @@
+package BackJoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B_17413_WordReverse2_2 {
+    Stack<Character> wordStack = new Stack<>();
+    StringBuilder result = new StringBuilder();
+
+    public String solution(String input) {
+        int tagIndex = 0;
+        boolean isInAngleBracket = false;
+        for (int i = 0; i < input.length(); i++) {
+            char inputChar = input.charAt(i);
+
+            if(inputChar == '<') {
+                appendWord();
+                tagIndex = i;
+                isInAngleBracket = true;
+            }else if(inputChar == '>'){
+                String substring;
+
+                if(i == input.length() -1) substring = input.substring(tagIndex);
+                else substring = input.substring(tagIndex, i+1);
+
+                result.append(substring);
+                tagIndex = 0;
+                isInAngleBracket = false;
+            }else {
+                if(!isInAngleBracket) {
+                    if(inputChar == ' ') {
+                        appendWord();
+                        result.append(" ");
+                    }else {
+                        wordStack.push(inputChar);
+                    }
+                }
+            }
+
+        }
+        appendWord();
+        return result.toString();
+    }
+    public void appendWord() {
+        while(!wordStack.empty()) {
+            result.append(wordStack.pop());
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        B_17413_WordReverse2_2 reverseWord = new B_17413_WordReverse2_2();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String solution = reverseWord.solution(reader.readLine());
+        System.out.println(solution);
+    }
+}
