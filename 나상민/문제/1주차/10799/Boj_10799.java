a08a47fdc2f4370d1753bba9d36e625c33c7b723
Sangmin <sm9171@nate.com>
2022-11-20 10:37:18 +0900

    ➕ [ADD] : 1주차 10799, 17413 문제 풀이

---
+++ 나상민/문제/1주차/10799/Boj_10799.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Boj_10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push(input.charAt(i));
+            } else {
+                stack.pop();
+                if (input.charAt(i - 1) == '(') {
+                    result += stack.size();
+                } else {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
