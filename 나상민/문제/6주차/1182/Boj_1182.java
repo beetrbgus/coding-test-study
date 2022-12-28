b865bc7754fc3412c412201fdb311f9f3d8b4bdb
Sangmin <sm9171@nate.com>
2022-12-28 23:26:23 +0900

    ➕ [ADD] : 6주차 1182 문제 풀이

---
+++ 나상민/문제/6주차/1182/Boj_1182.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Boj_1182 {
+    static int[] arr;
+    static int n;
+    static int cnt;
+    static int s;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        backTrack(0, 0, true);
+        System.out.println(cnt);
+    }
+    private static void backTrack(int k, int sum, boolean gong) {
+
+        if (k == n) {
+            if (sum == s && !gong) {
+                cnt++;
+            }
+            return;
+        }
+
+        backTrack(k+1, sum, gong);
+        backTrack(k+1, sum+arr[k], false);
+
+    }
+}
